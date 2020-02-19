592b4253e701d1e485a1417211278590eb10d0b6
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-19 12:15:17 -0600

    add weather.router.js file for http requests

--- server/server.js
+++ server/server.js
@@ -2,13 +2,14 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 5000;
+const weather = require('./routes/weather.router.js');
 
 /** ---------- MIDDLEWARE ---------- **/
 app.use(bodyParser.json()); // needed for axios requests
 app.use(express.static('build'));
 
 /** ---------- EXPRESS ROUTES ---------- **/
-
+app.use('/weather', weather);
 
 /** ---------- START SERVER ---------- **/
 app.listen(PORT,  () => {
