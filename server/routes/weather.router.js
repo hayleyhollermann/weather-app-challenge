6935459edbb54b03f7bd22aa21611f59b89d6b7e
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-21 12:19:47 -0600

    set up get route to retrieve info from API

--- server/routes/weather.router.js
+++ server/routes/weather.router.js
@@ -1,7 +1,22 @@
 const express = require('express');
 const router = express.Router();
-const pool = require('../modules/pool.js');
+const axios = require('axios');
 
+router.get('/', (req, res) => {
+    console.log('hitting route');
+    let key = process.env.WEATHER_API_KEY;
+    let city = 55305;
+    axios({
+        method: 'GET',
+        url: `http://api.weatherapi.com/v1/forecast.json?key=${key}&q=${city}&days=3`
+    }) .then((response) => {
+        console.log(response);
+        res.send(response.data)
+    }) .catch((err) => {
+        console.log("error in /weather:", err);
+        res.sendStatus(500)
+    })
+})
 
 
 module.exports = router;
