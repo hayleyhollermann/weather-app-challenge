23b48d40f0fee28f129acc3a629bfe536c69bab6
Hayley Hollermann <hayleyhollermann@gmail.com>
2020-02-24 13:50:28 -0600

    Updates

---
+++ precache-manifest.2aa80e694c31b4c56a940c6cfe6e353c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d85646a1e97e29cf5785c2db7efc555",
+    "url": "/weather-app-challenge/index.html"
+  },
+  {
+    "revision": "059f2a4eb7ab82a8ad7f",
+    "url": "/weather-app-challenge/static/css/main.60ec4904.chunk.css"
+  },
+  {
+    "revision": "ae72ef2746ea57247998",
+    "url": "/weather-app-challenge/static/js/2.d8f8ac19.chunk.js"
+  },
+  {
+    "revision": "3adc01bea76e7956dc3633ee898f6936",
+    "url": "/weather-app-challenge/static/js/2.d8f8ac19.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "059f2a4eb7ab82a8ad7f",
+    "url": "/weather-app-challenge/static/js/main.25368554.chunk.js"
+  },
+  {
+    "revision": "61a7fc73aff6e08aa46a",
+    "url": "/weather-app-challenge/static/js/runtime-main.fc13d615.js"
+  }
+]);
